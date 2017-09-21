250461fe8dc401b4920021dcc6bb3e7f1be447d3
Pedro Guilherme Rohr <pedro.guilherme.rohr@gmail.com>
2017-09-21 18:27:25 -0300

    feat(crescer): initial commit
    Create project structure using Angular CLI: ng new Crescer --directory crescer-cwi --prefix crescer --routing true

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('crescer App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to crescer!');
+  });
+});
