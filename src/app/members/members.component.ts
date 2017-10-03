ce429027799edf6d611a0f4d2f49297114efba05
Pedro Guilherme Rohr <pedro.guilherme.rohr@gmail.com>
2017-10-03 20:36:21 -0300

    feat(members): add search field to filter members

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,6 +1,8 @@
-import { Student } from './../common/students/student.model';
 import { Component, OnInit } from '@angular/core';
 
+import { Instructor } from './../common/instructors/instructor.model';
+import { InstructorService } from './../common/instructors/instructor.service';
+import { Student } from './../common/students/student.model';
 import { StudentService } from './../common/students/student.service';
 
 @Component({
@@ -11,11 +13,32 @@ import { StudentService } from './../common/students/student.service';
 export class MembersComponent implements OnInit {
 
   public students: Student[];
+  public instructors: Instructor[];
+  public filteredStudents: Student[];
+  public filteredInstructors: Instructor[];
+  public searchIconFocus: boolean;
 
-  constructor(private studentService: StudentService) { }
+  constructor(private studentService: StudentService, private instructorService: InstructorService) { }
 
+  public toggleSearchIcon(): void {
+    this.searchIconFocus = !this.searchIconFocus;
+  }
+
+  public searchMember(searchText: string): void {
+    const lowerCasedSearchText = searchText.toLocaleLowerCase();
+    this.filteredStudents = this.students.filter(
+          student => (student.firstName + ' ' + student.lastName)
+                      .toLocaleLowerCase().indexOf(lowerCasedSearchText) > -1
+    );
+    this.filteredInstructors = this.students.filter(
+      instructor => (instructor.firstName + ' ' + instructor.lastName)
+                  .toLocaleLowerCase().indexOf(lowerCasedSearchText) > -1
+);
+}
   ngOnInit() {
-    this.students = this.studentService.getStudents();
+    this.students = this.filteredStudents = this.studentService.getStudents();
+    this.instructors = this.filteredInstructors = this.instructorService.getInstructors();
+    this.searchIconFocus = false;
   }
 
 }
