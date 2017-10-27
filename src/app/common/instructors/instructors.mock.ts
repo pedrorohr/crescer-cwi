c0271421edfc7698c83d6c3c99e319e071aab270
Leonardo Bork <leonardo.bork@cwi.com.br>
2017-10-27 08:18:35 -0200

    fix(merge): fix merge problems

--- src/app/common/instructors/instructors.mock.ts
+++ src/app/common/instructors/instructors.mock.ts
@@ -6,9 +6,5 @@ export const instructors: Instructor[] = [
     { id: 102, firstName: 'Ben-hur', lastName: 'Ott', user: 'ben-hur', phone: '5551981461003', gravatar: '3685827c7e77a3e9a79d8d1977238aee', position: -1 },
     { id: 103, firstName: 'André', lastName: 'Nunes', user: 'andrenunes', phone: '5551981751020', gravatar: 'd772d2c444377a4062eac9d00c1a9d32', position: -1 },
     { id: 104, firstName: 'Bernardo', lastName: 'Rezende', user: 'bernardobrezende', phone: '5551992342304', gravatar: '4ebf761c602ecc1829015486742f1288', position: -1 },
-<<<<<<< HEAD
-    { id: 105, firstName: 'Everton', lastName: 'Zanatta', user: 'zanatta.everton', phone: '5551982433788', gravatar: '84918f4dc0c77d1e92da8b617d429af8', position: 100 }
-=======
     { id: 105, firstName: 'Everton', lastName: 'Zanatta', user: 'zanatta.everton', phone: '5551982433788', gravatar: '84918f4dc0c77d1e92da8b617d429af8', position: -1 }
->>>>>>> upstream/master
 ];
